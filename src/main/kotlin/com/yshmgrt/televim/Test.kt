0e8380fac1920692ea8ecfabb1f0eea171320513
LDS2002 <fireman2002lds@gmail.com>
2019-10-27 15:08:34 +0300

    Delete junk file

--- src/main/kotlin/com/yshmgrt/televim/Test.kt
+++
@@ -1,9 +0,0 @@
-package com.yshmgrt.televim
-
-import com.yshmgrt.televim.controller.Controller
-
-fun main(args: Array<String>) {
-    val c = Controller()
-    val res = c.onCommand(":login -p +79118388834 -l -pd asd da f d")
-    print(res)
-}
